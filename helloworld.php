05a48ddd2c11b9bffe195804e8babe4d35d9c3c8
Sam Walton <samuelwalton@users.noreply.github.com>
2016-07-18 15:26:46 -0500

    Create helloworld.php
    init

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<?php
+
+// PHP Hello World Example
+// Sam Walton <sam@seven-labs.com> 
+// 07/18/2016
+
+// Write output to console or output
+echo "Hello World";
+
+// Print output to console or output with another native function
+print('hello world');
+
+?>
